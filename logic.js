f757f2f4d959e801c4bba896501d2a88a404c027
AstraKult <Keanui.beauchesne@gmail.com>
2025-12-06 11:19:15 +0100

    remake

--- logic.js
+++ logic.js
@@ -1,18 +1,19 @@
-const btn_view_works = document.getElementById("btn_view_works")
-
-btn_view_works.addEventListener("click", function () {
-    window.scrollTo({
-        top: 900,
-        left: 0,
-        behavior: "smooth"
-    });
-})
 
 const btn_view_social_proofs = document.getElementById("btn_view_social_proofs")
 
 btn_view_social_proofs.addEventListener("click", function () {
     window.scrollTo({
-        top: 500,
+        top: 1350,
+        left: 0,
+        behavior: "smooth"
+    });
+})
+
+const btn_view_works = document.getElementById("btn_view_works")
+
+btn_view_works.addEventListener("click", function () {
+    window.scrollTo({
+        top: 2050,
         left: 0,
         behavior: "smooth"
     });
